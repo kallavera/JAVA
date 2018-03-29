a01582e9e979a14b80d4deccaf78bd00dabd7b19
Ariel Velasco <arield.velasco@gmail.com>
2018-03-29 18:29:39 -0300

    Init

---
+++ LWJGL3_2D_Game/src/com/elegantwhelp/tutorial/core/Launcher.java
@@ -0,0 +1,9 @@
+package com.elegantwhelp.tutorial.core;
+
+public class Launcher
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello ElegantWhelp");
+	}
+}
---
+++ LWJGL3_3D_Game/src/com/thinmatrix/tutorial/core/Launcher.java
@@ -0,0 +1,9 @@
+package com.thinmatrix.tutorial.core;
+
+public class Launcher
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello ThinMatrix LWJGL3");
+	}
+}
