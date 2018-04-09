6cba0b8a29ccb0a597a23a134a593ac83d53425e
Ariel Velasco <arield.velasco@gmail.com>
2018-04-09 07:33:46 -0300

    Video 01

---
+++ LWJGL2_3D_Game/src/engineTester/MainGameLoop.java
@@ -0,0 +1,27 @@
+package engineTester;
+
+import org.lwjgl.opengl.Display;
+
+import renderEngine.DisplayManager;
+
+public class MainGameLoop
+{
+
+	public MainGameLoop()
+	{
+		
+	}
+
+	public static void main(String[] args)
+	{
+		DisplayManager.createDisplay();
+		
+		while(!Display.isCloseRequested())
+		{
+			DisplayManager.updateDisplay();
+		}
+		
+		DisplayManager.closeDisplay();
+	}
+
+}
--- LWJGL2_3D_Game/src/renderEngine/DisplayManager.java
+++ LWJGL2_3D_Game/src/renderEngine/DisplayManager.java
@@ -1,20 +1,17 @@
-package com.thinmatrix.tutorial.core;
+package renderEngine;
 
 import static org.lwjgl.opengl.GL11.*;
 
 import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.ContextAttribs;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.PixelFormat;
+import org.lwjgl.opengl.*;
 
 public class DisplayManager
 {
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 600;
-	public static final int FPS_CAP = 60;
+	public static final int FPS_CAP = 120;
 	
-	public static final String TITLE = "LWJGL2 3D Tutorial";
+	public static final String TITLE = "LWJGL Tutorial";
 
 	public DisplayManager()
 	{
@@ -24,14 +21,13 @@ public class DisplayManager
 	public static void createDisplay()
 	{
 		ContextAttribs attribs = new ContextAttribs(3,2)
-				.withForwardCompatible(true)
-				.withProfileCore(true);
+		.withForwardCompatible(true)
+		.withProfileCore(false);
 		
 		try
 		{
 			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
 			Display.create(new PixelFormat(), attribs);
-			Display.setVSyncEnabled(true);
 			Display.setTitle(TITLE);
 		}
 		catch (LWJGLException e)
@@ -45,12 +41,13 @@ public class DisplayManager
 	public static void updateDisplay()
 	{
 		Display.sync(FPS_CAP);
+		Display.setVSyncEnabled(true);
 		Display.update();
+		
 	}
 	
 	public static void closeDisplay()
 	{
 		Display.destroy();
 	}
-
 }
